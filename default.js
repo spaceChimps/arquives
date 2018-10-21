b417426c7c46b337bdc57a930ae5c01b91f817c3
ericmizuta <44316873+ericmizuta@users.noreply.github.com>
2018-10-21 09:53:52 -0300

    v2.3 - map function OK

--- default.js
+++ default.js
@@ -1,15 +1,31 @@
-function myMap(maploc) {
-  console.log('maploc');
-  console.log(maploc);
-
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(28.5146281,-80.6289157), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
 
 
+
+
+
+
+$(document).ready(function(){
+
+  $(".login-toggle").click(function(){
+    $(".content-login-v2").toggle(400);
+  });
+
+  $(".ham").click(function(){
+    $("ul.nav.navbar-nav").toggle(400);
+    $(".navbar-header span p").toggle(50);
+  });
+
+ var cont1 = $( "p.infocord" );
+        var arry = [];
+
+        for ( var i = 1; i < cont1.length + 1; i++ ) {
+            arry.push({
+               lat: parseFloat($("p.infocord:nth-of-type(" + i + ")").text()),
+               lng: parseFloat($("span.cord2:nth-of-type(" + i + ")").text())
+            });
+          }
+
+        console.log(arry)
 
 //Get user location 
 
@@ -22,51 +38,39 @@ function logPosition(position) {
 
   map = new google.maps.Map(document.getElementById('map'), {
   center: uluru,
-  zoom: 16
-	});
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-
-
-
-
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-
-
-
-$(document).ready(function(){
-  $(".login-toggle").click(function(){
-    $(".content-login-v2").toggle(400);
+  zoom: 14
   });
 
-  $(".ham").click(function(){
-    $("ul.nav.navbar-nav").toggle(400);
-    $(".navbar-header span p").toggle(50);
-  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+     map: map
+   });
+
+   // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = arry;
+
+
+
+
 
 });
-
-
-
-var cont1 = $( "p.infocord" );
-var arry = [];
-
-for ( var i = 1; i < cont1.length + 1; i++ ) {
-    arry.push({
-      a: $("p.infocord:nth-of-type(" + i + ")").text()
-    });
-  }
-
-console.log(arry)
-
 
 
 
